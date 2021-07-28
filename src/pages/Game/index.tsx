36d15b8b2d14889923790e0782987ed648f45338
laxative.hsieh <laxative.hsieh@yi-spaces.com>
2021-07-28 16:37:33 +0800

    Feat(Game): move randomCardList function outside

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -7,12 +7,12 @@ import './main.css';
 
 const CARD_LENGTH: number = 52;
 const ROW_NUMBER: number = 8;
-const isUsed: boolean[] = new Array(52).fill(false);
+const isUsed: boolean[] = new Array(CARD_LENGTH).fill(false);
 
 const getRandCardNum: Function = () => {
   let value: number;
   while (true) {
-    value = Math.floor(Math.random() * 52); // 0 - 51
+    value = Math.floor(Math.random() * CARD_LENGTH); // 0 - 51
     if (!isUsed[value]) {
       isUsed[value] = true;
       return value;
@@ -20,23 +20,24 @@ const getRandCardNum: Function = () => {
   }
 };
 
+const getRandomCardList: Function = (): number[][] => {
+  let list: number[][] = new Array(ROW_NUMBER).fill([])
+  for (let i = 0; i < CARD_LENGTH; ++i) {
+    let row: number = i % ROW_NUMBER;
+    let cardValue: number = getRandCardNum();
+    list[row] = [...list[row], cardValue];
+  }
+
+  return list;
+}
+
 const Game: React.FC = () => {
-  const [cardList, setCardList] = useState<number[][]>(new Array(8).fill([]));
+  const [cardList, setCardList] = useState<number[][]>(new Array(ROW_NUMBER).fill([]));
 
   // initialize cardList
   useEffect(() => {
-    const randomCardList = () => {
-      for (let i = 0; i < CARD_LENGTH; ++i) {
-        let row: number = i % ROW_NUMBER;
-        let cardValue: number = getRandCardNum();
-        setCardList(function (prevValue: number[][]) {
-          let copy: number[][] = [...prevValue];
-          copy[row] = [...copy[row], cardValue];
-          return copy;
-        });
-      }
-    };
-    randomCardList();
+    const randomCardList: number[][] = getRandomCardList()
+    setCardList(randomCardList);
   }, []);
 
   return (
